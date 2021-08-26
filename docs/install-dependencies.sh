1b6892d845919ce683971f0295488e77d94dbe18
Blaz Dular <blaz.dular@gmail.com>
2021-08-26 21:35:59 +0200

    Fixing comments and installing Go with an external program

--- docs/install-dependencies.sh
+++ docs/install-dependencies.sh
@@ -3,26 +3,26 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# download libnfc
+# Download libnfc
 cd ~ && mkdir libnfc
 cd libnfc/
 wget https://github.com/nfc-tools/libnfc/releases/download/libnfc-1.8.0/libnfc-1.8.0.tar.bz2
 tar -xvjf libnfc-1.8.0.tar.bz2
 cd libnfc-1.8.0 && sudo mkdir /etc/nfc /etc/nfc/devices.d
-# add configuration file
+# Add configuration for PN532_I2C
 touch /etc/nfc/devices.d/pn532_i2c.conf
 echo name = "PN532 board via I2C" >>/etc/nfc/devices.d/pn532_i2c.conf
 echo connstring = pn532_i2c:/dev/i2c-1 >>/etc/nfc/devices.d/pn532_i2c.conf
 echo allow_intrusive_scan = true >>/etc/nfc/devices.d/pn532_i2c.conf
 
-#install dependencies
+# Configure and install libnfc
 sudo apt-get install autoconf libtool libpcsclite-dev libusb-dev -y
 autoreconf -vis
 ./configure --with-drivers=pn532_i2c --sysconfdir=/etc --prefix=/usr
 sudo make clean
 sudo make install all
 
-# install WS281x drivers
+# Install WS281x drivers
 sudo apt-get install cmake -y
 cd ~
 git clone https://github.com/jgarff/rpi_ws281x
@@ -31,14 +31,10 @@ cd build
 cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON ..
 cmake --build .
 sudo make install
-cp *.a /usr/local/lib
-cp *.h /usr/local/include
+sudo cp *.a /usr/local/lib
+sudo cp *.h /usr/local/include
 
 if [ $1 -eq 1 ]; then
-  # install golang
-  export GOLANG="$(curl https://golang.org/dl/ | grep armv6l | grep -v beta | head -1 | awk -F\> {'print $3'} | awk -F\< {'print $1'})"
-  wget https://golang.org/dl/$GOLANG
-  sudo tar -C /usr/local -xzf $GOLANG
-  rm $GOLANG
-  unset GOLANG
+  # installing with the help of this: https://github.com/canha/golang-tools-install-script
+  wget -q -O - https://git.io/vQhTU | bash
 fi
