585cfdd37348d33485fc9219883054f22ca165df
xBlaz3kx <blaz.dular@gmail.com>
2025-07-13 09:59:47 +0200

    Improve script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
-# Update
-apt-get update -y
-apt-get upgrade -y
 
-# Install dependencies
-apt-get install cmake make autoconf libtool libpcsclite-dev libusb-dev bzip2 -y
+# Prepare the system for building libnfc and WS281x drivers
+apt-get update -y && apt-get install -y \
+    pkg-config \
+    build-essential \
+    cmake \
+    make \
+    autoconf \
+    libtool \
+    libpcsclite-dev \
+    libusb-dev \
+    bzip2 \
+    git \
+    wget \
+    gcc-aarch64-linux-gnu \
+    && rm -rf /var/lib/apt/lists/*
 
-# Download libnfc
-wget https://github.com/nfc-tools/libnfc/releases/download/libnfc-1.8.0/libnfc-1.8.0.tar.bz2
-tar -xvjf libnfc-1.8.0.tar.bz2
+# Download and install libnfc
+latest_tag=$(curl -s https://api.github.com/repos/nfc-tools/libnfc/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+wget "https://github.com/nfc-tools/libnfc/releases/download/${latest_tag}/libnfc-${latest_tag#libnfc-}.tar.bz2"
+tar -xvjf "libnfc-${latest_tag}.tar.bz2"
+cd "libnfc-${latest_tag}"
+make clean
+make install all
+
+# Configure libnfc
 mkdir -p /etc/nfc /etc/nfc/devices.d
-cd libnfc-1.8.0
 
 if [ "$1" = "pn532_i2c" ]; then
   # Add configuration for PN532_I2C
@@ -37,9 +52,6 @@ elif [ "$1" = "pn532_uart" ]; then
   ./configure --with-drivers=pn532_uart --enable-serial-autoprobe --sysconfdir=/etc --prefix=/usr
 fi
 
-make clean
-make install all
-
 # Install WS281x drivers
 cd ..
 git clone https://github.com/jgarff/rpi_ws281x
@@ -48,11 +60,7 @@ cd rpi_ws281x && mkdir -p build && cd build
 cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON ..
 cmake --build .
 make install
+
+# Copy libraries and headers to appropriate directories
 cp *.a /usr/local/lib
 cp *.h /usr/local/include
-
-# Optionally, install Go
-if [ "$2" -eq 1 ]; then
-  # installing Go with the help of this script: https://github.com/canha/golang-tools-install-script
-  wget -q -O - https://git.io/vQhTU | bash
-fi
